db2d97975b51fe21f4fc034ee817b6651cf8e4c7
Jihun Kim <zh1229@naver.com>
2025-06-29 23:19:02 +0900

    refactor(Menu): findAllByStoreId 메서드 findAllByStoreIdAndDeletedFalse로 변경

--- domain-menu/src/main/java/com/example/menu/repository/MenuRepository.java
+++ domain-menu/src/main/java/com/example/menu/repository/MenuRepository.java
@@ -1,6 +1,7 @@
 package com.example.menu.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,6 @@ import com.example.menu.entity.Menu;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-	List<Menu> findAllByStoreId(Long storeId);
+	List<Menu> findAllByStoreIdAndDeletedFalse(Long storeId);
+	Optional<Menu> findByStoreIdAndIdAndDeletedFalse(Long storeId, Long menuId);
 }
