9c431a13ea676c089f28934b9f3c2638b5c05599
Jihun Kim <zh1229@naver.com>
2025-06-29 23:23:35 +0900

    refactor(Store): 사용하지 않는 type 컬럼 삭제

--- api-admin/src/main/java/com/example/apiadmin/store/dto/StoreImageUploadResponse.java
+++ api-admin/src/main/java/com/example/apiadmin/store/dto/StoreImageUploadResponse.java
@@ -10,13 +10,11 @@ import lombok.Getter;
 public class StoreImageUploadResponse {
 	private final Long id;
 	private final String imageUrl;
-	private final String type;
 
 	public static StoreImageUploadResponse fromEntity(StoreImage storeImage) {
 		return StoreImageUploadResponse.builder()
 			.id(storeImage.getId())
 			.imageUrl(storeImage.getImageUrl())
-			.type(storeImage.getType())
 			.build();
 	}
 }
