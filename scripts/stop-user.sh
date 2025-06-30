c787006f233144a515c6be0431d491471abd3a18
Jihun Kim <zh1229@naver.com>
2025-06-30 18:30:59 +0900

    chore: CI/CD 스크립트 추가

--- scripts/stop-user.sh
+++ scripts/stop-user.sh
@@ -8,7 +8,7 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 STOP_LOG="$REPOSITORY/stop.log"
 
-$ chmod 666 "$STOP_LOG"
+$ chmod 666 STOP_LOG
 
 CURRENT_PID=$(pgrep -f ${APP_NAME}.*.jar)
 
