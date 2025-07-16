8410ed34e6d83afb322fa44998dcd905d1fa1de7
Jihun Kim <zh1229@naver.com>
2025-07-17 00:26:58 +0900

    feat(StoreImage): store 이미지 enum 타입 추가

---
+++ nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/store/entity/ImageType.java
@@ -0,0 +1,18 @@
+package com.nowait.domaincorerdb.store.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Schema(description = "이미지 타입 Enum")
+public enum ImageType {
+	@Schema(description = "주점 프로필 사진")
+	PROFILE("프로필 사진"),
+
+	@Schema(description = "주점 배너 사진")
+	BANNER("배너 사진");
+
+	private final String description;
+}
