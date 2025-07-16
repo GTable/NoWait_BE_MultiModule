a4a2b9beb4770cb172ae1f27068b4e2886c1761c
Jihun Kim <zh1229@naver.com>
2025-07-16 19:47:16 +0900

    refactor(Order): OrderCustomRepository 상속 제거

--- nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/order/repository/OrderRepository.java
+++ nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/order/repository/OrderRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.nowait.domaincorerdb.order.entity.UserOrder;
 
 @Repository
-public interface OrderRepository extends JpaRepository<UserOrder, Long>, OrderCustomRepository {
+public interface OrderRepository extends JpaRepository<UserOrder, Long> {
 	boolean existsBySignatureAndCreatedAtAfter(String signature, LocalDateTime createdAt);
 
 	List<UserOrder> findByStore_StoreIdAndTableIdAndSessionId(Long storeId, Long tableId, String sessionId);
