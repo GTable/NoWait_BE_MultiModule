8d053723979c950be80491921b55341633d42886
Jihun Kim <zh1229@naver.com>
2025-07-18 01:08:05 +0900

    refactor(Statistics): 유효성 검사 추가

--- nowait-event/src/main/java/com/nowait/nowaitevent/order/event/CookingCompleteEvent.java
+++ nowait-event/src/main/java/com/nowait/nowaitevent/order/event/CookingCompleteEvent.java
@@ -2,19 +2,28 @@ package com.nowait.nowaitevent.order.event;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class CookingCompleteEvent {
+
+	@NotNull
 	private final Long storeId;
+
+	@NotNull
 	private final List<Item> items;
 
 	@Getter
 	@RequiredArgsConstructor
 	public static class Item {
+
+		@NotNull
 		private final Long menuId;
+
+		@NotNull
 		private final int quantity;
 	}
 }
