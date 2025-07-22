bcbc6fb103735df727bdbac8647c5a26007d6292
Jihun Kim <zh1229@naver.com>
2025-07-22 18:29:53 +0900

    refactor(rank): 매출 데이터 없는 경우 redis 초기화 로직 추가

--- nowait-app-admin-api/src/main/java/com/nowait/applicationadmin/statistic/scheduler/RankingRefreshScheduler.java
+++ nowait-app-admin-api/src/main/java/com/nowait/applicationadmin/statistic/scheduler/RankingRefreshScheduler.java
@@ -35,7 +35,7 @@ public class RankingRefreshScheduler {
 		refresh();
 	}
 
-	@Scheduled(cron = "0 */5 * * * *") // 매 5분마다 실행
+	@Scheduled(cron = "*/30 * * * * *") // 매 5분마다 실행
 	public void refresh() {
 		log.info("RankingRefreshScheduler.refresh() called at {}", LocalDateTime.now());
 
@@ -62,7 +62,6 @@ public class RankingRefreshScheduler {
 			log.warn("매출 데이터가 없습니다. 다음 스냅샷 키를 초기화합니다.");
 			redis.delete(currentKey);
 			redis.delete(previousKey);
-			redis.delete(nextKey);
 
 			return;
 		}
