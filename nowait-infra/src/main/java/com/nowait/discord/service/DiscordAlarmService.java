c31771dfed631e0aa5231ec64987a8ee8bfad91b
Jihun Kim <zh1229@naver.com>
2025-07-28 11:28:30 +0900

    refactor: 배포 환경에서만 알림 가도록 설정

--- nowait-infra/src/main/java/com/nowait/discord/service/DiscordAlarmService.java
+++ nowait-infra/src/main/java/com/nowait/discord/service/DiscordAlarmService.java
@@ -3,8 +3,10 @@ package com.nowait.discord.service;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
@@ -24,41 +26,42 @@ public class DiscordAlarmService {
 
 	private final DiscordClientUser discordClientUser;
 	private final DiscordClientAdmin discordClientAdmin;
+	private final Environment environment;
 
 	public void sendDiscordUserAlarm(Exception e, WebRequest request) {
-		discordClientUser.sendAlarm(createMessage(e, request));
+		if (shouldNotifyUser()) {
+			discordClientUser.sendAlarm(createMessage(e, request));
+		}
 	}
 
 	public void sendDiscordAdminAlarm(Exception e, WebRequest request) {
-		discordClientAdmin.sendAlarm(createMessage(e, request));
+		if (shouldNotifyAdmin()) {
+			discordClientAdmin.sendAlarm(createMessage(e, request));
+		}
+	}
+
+	private boolean shouldNotifyAdmin() {
+		return Arrays.stream(environment.getActiveProfiles()).noneMatch("admin-local"::equals);
+	}
+
+	private boolean shouldNotifyUser() {
+		return Arrays.stream(environment.getActiveProfiles()).noneMatch("user-local"::equals);
 	}
 
 	private DiscordMessage createMessage(Exception e, WebRequest request) {
 		return DiscordMessage.builder()
 			.content("# 🚨 에러 발생 비이이이이사아아아앙")
-			.embeds(
-				List.of(
-					DiscordMessage.Embed.builder()
-						.title("ℹ️ 에러 정보")
-						.description(
-							"### 🕖 발생 시간\n"
-							+ LocalDateTime.now()
-							+ "\n"
-							+ "### 🔗 요청 URL\n"
-							+ createRequestFullPath(request)
-							+ "\n"
-							+ "### 📄 Stack Trace\n"
-							+ "```\n"
-							+ getStackTrace(e).substring(0, 1000)
-							+ "\n```")
-						.build()
-				)
-			)
+			.embeds(List.of(DiscordMessage.Embed.builder()
+				.title("ℹ️ 에러 정보")
+				.description(
+					"### 🕖 발생 시간\n" + LocalDateTime.now() + "\n" + "### 🔗 요청 URL\n" + createRequestFullPath(request)
+					+ "\n" + "### 📄 Stack Trace\n" + "```\n" + getStackTrace(e).substring(0, 1000) + "\n```")
+				.build()))
 			.build();
 	}
 
 	private String createRequestFullPath(WebRequest webRequest) {
-		HttpServletRequest request = ((ServletWebRequest) webRequest).getRequest();
+		HttpServletRequest request = ((ServletWebRequest)webRequest).getRequest();
 		String fullPath = request.getMethod() + " " + request.getRequestURL();
 		String queryString = request.getQueryString();
 		if (queryString != null) {
