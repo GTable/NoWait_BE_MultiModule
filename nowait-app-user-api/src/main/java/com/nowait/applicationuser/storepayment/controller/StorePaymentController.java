98d33070a7d6cf1dff992a6eca047822bbb53e48
Jihun Kim <zh1229@naver.com>
2025-07-31 16:27:43 +0900

    refactor(storePayment): 결제 데이터 없는 경우 204 반환하도록 변경

--- nowait-app-user-api/src/main/java/com/nowait/applicationuser/storepayment/controller/StorePaymentController.java
+++ nowait-app-user-api/src/main/java/com/nowait/applicationuser/storepayment/controller/StorePaymentController.java
@@ -1,5 +1,7 @@
 package com.nowait.applicationuser.storepayment.controller;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nowait.applicationuser.storepayment.dto.StorePaymentReadDto;
 import com.nowait.applicationuser.storepayment.service.StorePaymentService;
 import com.nowait.common.api.ApiUtils;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,12 +32,25 @@ public class StorePaymentController {
 	@Operation(summary = "주점 결제 정보 조회", description = "주점 ID로 주점 결제 정보를 조회합니다.")
 	@ApiResponse(responseCode = "200", description = "주점 결제 정보 조회 성공")
 	public ResponseEntity<?> getStorePaymentByStoreId(@PathVariable Long storeId) {
-		return ResponseEntity
-			.status(HttpStatus.OK)
-			.body(
-				ApiUtils.success(
-					storePaymentService.getStorePaymentByStoreId(storeId)
-				)
-			);
+		Optional<StorePaymentReadDto> response = storePaymentService.getStorePaymentByStoreId(storeId);
+
+		if (response.isPresent()) {
+			return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(
+					ApiUtils.success(
+						response
+					)
+				);
+		} else {
+			return ResponseEntity
+				.status(HttpStatus.NO_CONTENT)
+				.body(
+					ApiUtils.success(
+						response
+					)
+				);
+		}
+
 	}
 }
