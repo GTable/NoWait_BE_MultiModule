96527417d63dd9b41adf93e9747631af0bf815e2
Jihun Kim <zh1229@naver.com>
2025-08-08 21:19:33 +0900

    refactor(Order): 입력값 검증 추가

--- nowait-app-admin-api/src/main/java/com/nowait/applicationadmin/menu/dto/MenuSortUpdateRequest.java
+++ nowait-app-admin-api/src/main/java/com/nowait/applicationadmin/menu/dto/MenuSortUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.nowait.applicationadmin.menu.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MenuSortUpdateRequest {
+	@NotNull
 	private Long menuId;
+	@NotNull
+	@PositiveOrZero
 	private Long sortOrder;
 }
