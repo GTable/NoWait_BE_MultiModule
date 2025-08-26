5f48a2a9c95f59fd2c36c0f8b75a64298e6da491
Jihun Kim <zh1229@naver.com>
2025-08-26 18:53:30 +0900

    refactor: findByStore_PublicCodeAndTableIdAndSessionId 변경

--- nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/order/repository/OrderRepository.java
+++ nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/order/repository/OrderRepository.java
@@ -13,7 +13,7 @@ import com.nowait.domaincorerdb.order.entity.UserOrder;
 public interface OrderRepository extends JpaRepository<UserOrder, Long> {
 	boolean existsBySignatureAndCreatedAtAfter(String signature, LocalDateTime createdAt);
 
-	List<UserOrder> findByStore_StoreIdAndTableIdAndSessionId(Long storeId, Long tableId, String sessionId);
+	List<UserOrder> findByStore_PublicCodeAndTableIdAndSessionId(String publicCode, Long tableId, String sessionId);
 
 	@EntityGraph(attributePaths = {"orderItems", "orderItems.menu"})
 	List<UserOrder> findAllByStore_StoreIdAndCreatedAtBetween(Long storeId, LocalDateTime startDateTime, LocalDateTime endDateTime);
