fea331863736e8646cf83144dd448d3000d53e6d
Jihun Kim <zh1229@naver.com>
2025-09-02 17:33:15 +0900

    refactor: 메뉴 이미지 업로드 로직 리팩토링

--- nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/menu/repository/MenuImageRepository.java
+++ nowait-domain/domain-core-rdb/src/main/java/com/nowait/domaincorerdb/menu/repository/MenuImageRepository.java
@@ -1,6 +1,7 @@
 package com.nowait.domaincorerdb.menu.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import com.nowait.domaincorerdb.menu.entity.MenuImage;
 @Repository
 public interface MenuImageRepository extends JpaRepository<MenuImage, Long> {
 	List<MenuImage> findByMenu(Menu menu);
+	Optional<MenuImage> findByMenuId(Long menuId);
 }
