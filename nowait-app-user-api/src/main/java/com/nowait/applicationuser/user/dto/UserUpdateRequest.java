faabc3146061878370b48ef01f28945ee3da79d9
Jihun Kim <zh1229@naver.com>
2025-09-05 15:03:59 +0900

    refactor: NPE 방지를 위한 primitive boolean으로 변경

--- nowait-app-user-api/src/main/java/com/nowait/applicationuser/user/dto/UserUpdateRequest.java
+++ nowait-app-user-api/src/main/java/com/nowait/applicationuser/user/dto/UserUpdateRequest.java
@@ -7,4 +7,4 @@ public record UserUpdateRequest(
 	@NotBlank
 	@Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "휴대폰 번호는 010-0000-0000 형식이어야 합니다.")
 	String phoneNumber,
-	Boolean consent) { }
+	boolean consent) { }
