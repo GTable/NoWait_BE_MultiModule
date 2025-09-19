c40461d8d9453993b6ae01f8049f5b37843a8a0e
Jihun Kim <zh1229@naver.com>
2025-09-19 18:50:02 +0900

    refactor: 유저 검증 메서드 분리

--- nowait-app-admin-api/src/main/java/com/nowait/applicationadmin/store/service/StoreServiceImpl.java
+++ nowait-app-admin-api/src/main/java/com/nowait/applicationadmin/store/service/StoreServiceImpl.java
@@ -130,7 +130,7 @@ public class StoreServiceImpl implements StoreService {
 	@Transactional
 	public Boolean toggleActive(Long storeId) {
 		Store store = storeRepository.findById(storeId)
-			.orElseThrow(() -> new IllegalArgumentException("해당 스토어가 존재하지 않습니다."));
+			.orElseThrow(StoreNotFoundException::new);
 
 		store.toggleActive();
 		return store.getIsActive();
