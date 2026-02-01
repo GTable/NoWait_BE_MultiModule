e40ee5d658c0b84070a357b65d0c9a963fde0d85
Jihun Kim <zh1229@naver.com>
2026-02-01 21:38:03 +0900

    feat: 웨이팅 취소 로직 추가 및 멱등성 처리 로직 추가

--- nowait-app-user-api/src/main/java/com/nowait/applicationuser/waiting/controller/WaitingController.java
+++ nowait-app-user-api/src/main/java/com/nowait/applicationuser/waiting/controller/WaitingController.java
@@ -2,12 +2,15 @@ package com.nowait.applicationuser.waiting.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nowait.applicationuser.waiting.dto.CancelWaitingRequest;
+import com.nowait.applicationuser.waiting.dto.CancelWaitingResponse;
 import com.nowait.applicationuser.waiting.dto.RegisterWaitingRequest;
 import com.nowait.applicationuser.waiting.dto.RegisterWaitingResponse;
 import com.nowait.applicationuser.waiting.service.WaitingService;
@@ -16,11 +19,12 @@ import com.nowait.domainuserrdb.oauth.dto.CustomOAuth2User;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Waiting API", description = "예약 API -리팩토링 중-")
 @RestController
-@RequestMapping("/v1/users/me/waitings-refactoring")
+@RequestMapping("/v2/users/me/waitings")
 @RequiredArgsConstructor
 public class WaitingController {
 	private final WaitingService waitingService;
@@ -28,17 +32,19 @@ public class WaitingController {
 	/**
 	 * 대기열 리팩토링용 API
 	 */
-	@PostMapping("/progress/{storeId}")
+	@PostMapping("/{publicCode}")
 	@Operation(summary = "대기열 리팩토링용 API", description = "대기열 리팩토링용 API")
 	public ResponseEntity<?> registerWaiting(
 		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@PathVariable Long storeId,
-		@RequestBody RegisterWaitingRequest request
+		@PathVariable String publicCode,
+		@RequestBody RegisterWaitingRequest request,
+		HttpServletRequest httpServletRequest
 	) {
 		RegisterWaitingResponse registerWaitingResponse = waitingService.registerWaiting(
 			customOAuth2User,
-			storeId,
-			request
+			publicCode,
+			request,
+			httpServletRequest
 		);
 
 		return ResponseEntity
@@ -46,6 +52,28 @@ public class WaitingController {
 			.body(
 				ApiUtils.success(
 					registerWaitingResponse
+				)
+			);
+	}
+
+	@DeleteMapping("/{publicCode}")
+	@Operation(summary = "대기열 리팩토링용 API", description = "대기 취소")
+	public ResponseEntity<?> cancelWaiting(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+		@PathVariable String publicCode,
+		@RequestBody CancelWaitingRequest request
+	) {
+		CancelWaitingResponse cancelWaitingResponse = waitingService.cancelWaiting(
+			customOAuth2User,
+			publicCode,
+			request
+		);
+
+		return ResponseEntity
+			.ok()
+			.body(
+				ApiUtils.success(
+					cancelWaitingResponse
 				)
 			);
 	}
--- nowait-app-user-api/src/main/java/com/nowait/applicationuser/waiting/service/WaitingService.java
+++ nowait-app-user-api/src/main/java/com/nowait/applicationuser/waiting/service/WaitingService.java
@@ -2,16 +2,22 @@ package com.nowait.applicationuser.waiting.service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nowait.applicationuser.waiting.dto.CancelWaitingRequest;
+import com.nowait.applicationuser.waiting.dto.CancelWaitingResponse;
 import com.nowait.applicationuser.waiting.dto.RegisterWaitingRequest;
 import com.nowait.applicationuser.waiting.dto.RegisterWaitingResponse;
+import com.nowait.applicationuser.waiting.dto.WaitingIdempotencyValue;
 import com.nowait.applicationuser.waiting.event.AddWaitingRegisterEvent;
+import com.nowait.applicationuser.waiting.redis.WaitingIdempotencyRepository;
 import com.nowait.common.enums.ReservationStatus;
 import com.nowait.domaincorerdb.reservation.entity.Reservation;
+import com.nowait.domaincorerdb.reservation.exception.ReservationNotFoundException;
 import com.nowait.domaincorerdb.reservation.repository.ReservationRepository;
 import com.nowait.domaincorerdb.store.entity.Store;
 import com.nowait.domaincorerdb.store.exception.StoreNotFoundException;
@@ -23,10 +29,13 @@ import com.nowait.domaincoreredis.common.util.RedisKeyUtils;
 import com.nowait.domaincoreredis.reservation.repository.WaitingRedisRepository;
 import com.nowait.domainuserrdb.oauth.dto.CustomOAuth2User;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WaitingService {
 
 	private final ReservationRepository reservationRepository;
@@ -34,30 +43,44 @@ public class WaitingService {
 	private final StoreRepository storeRepository;
 	private final UserRepository userRepository;
 	private final ApplicationEventPublisher eventPublisher;
+	private final WaitingIdempotencyRepository waitingIdempotencyRepository;
 
 	/**
 	 * 최초 대기 등록
-	 * @param storeId
+	 * @param publicCode
 	 * @param waitingRequest
 	 */
 	// 대기열 리팩토링 서비스 메서드
 	@Transactional
-	public RegisterWaitingResponse registerWaiting(CustomOAuth2User oAuth2User, Long storeId, RegisterWaitingRequest waitingRequest) {
+	public RegisterWaitingResponse registerWaiting(CustomOAuth2User oAuth2User, String publicCode, RegisterWaitingRequest waitingRequest, HttpServletRequest httpServletRequest) {
 
 		// TODO 유저 및 주점 존재 검증은 공통으로 많이 쓰이니 AOP로 빼는게 좋을 듯
-		Store store = storeRepository.findById(storeId)
+		Store store = storeRepository.findByPublicCodeAndDeletedFalse(publicCode)
 			.orElseThrow(StoreNotFoundException::new);
 
 		User user = userRepository.findById(oAuth2User.getUserId())
 			.orElseThrow(UserNotFoundException::new);
 
 		// 웨이팅 고유 번호 생성 - YYYYMMDD-storeId-sequence number 일련 번호
+		Long storeId = store.getStoreId();
 		LocalDateTime timestamp = LocalDateTime.now();
 		String waitingNumber = generateWaitingNumber(storeId, timestamp);
-		String idempotentKey = generateIdempotentKey(storeId, user.getId());
+		String idempotentKey = httpServletRequest.getHeader("Idempotency-Key");
 
 		// 멱등키 검증 - 이미 동일한 멱등키로 등록된 웨이팅이 있는지 확인
-		waitingRedisRepository.indempotencyKeyExists(idempotentKey, ReservationStatus.WAITING.name());
+		// waitingRedisRepository.idempotentKeyKeyExists(idempotentKey, ReservationStatus.WAITING.name());
+
+		// TODO 멱등성 검증 로직 점검 필요
+		Optional<WaitingIdempotencyValue> existingIdempotencyValue = waitingIdempotencyRepository.findByKey(idempotentKey);
+		if (existingIdempotencyValue.isPresent()) {
+			log.info("Existing idempotency key found: {}", idempotentKey);
+			return existingIdempotencyValue.get().getResponse();
+		}
+
+
+		// 일일 가능 웨이팅 최대 개수 초과 검증
+		// TODO race condition 발생 가능성 점검 필요
+		waitingRedisRepository.incrementAndCheckWaitingLimit(user.getId(), 3L);
 
 		// DB에 상태 값 저장
 		Reservation reservation = Reservation.builder()
@@ -81,30 +104,46 @@ public class WaitingService {
 			)
 		);
 
-		return RegisterWaitingResponse.builder()
+		RegisterWaitingResponse response = RegisterWaitingResponse.builder()
 			.waitingNumber(waitingNumber)
 			.partySize(waitingRequest.getPartySize())
 			.build();
+
+		// 멱등키가 있다면 멱등 응답 저장
+		waitingIdempotencyRepository.saveIdempotencyValue(idempotentKey, response);
+
+		return response;
 	}
 
 	@Transactional
-	public void cancelWaiting(CustomOAuth2User oAuth2User, Long storeId) {
-		// TODO 유저 및 주점 존재 검증은 공통으로 많이 쓰이니 AOP로 빼는게 좋을 듯
-		Store store = storeRepository.findById(storeId)
-			.orElseThrow(StoreNotFoundException::new);
+	public CancelWaitingResponse cancelWaiting(CustomOAuth2User oAuth2User, String publicCode, CancelWaitingRequest request) {
+
+		Store store = storeRepository.findByPublicCodeAndDeletedFalse(publicCode).orElseThrow(StoreNotFoundException::new);
+		Long storeId = store.getStoreId();
 
 		User user = userRepository.findById(oAuth2User.getUserId())
 			.orElseThrow(UserNotFoundException::new);
 
-		String idempotentKey = generateIdempotentKey(storeId, user.getId());
-		waitingRedisRepository.indempotencyKeyExists(idempotentKey, ReservationStatus.CANCELLED.name());
+		// TODO 멱등키 검증 로직 점검 필요
+		// String idempotentKey = generateIdempotentKey(storeId, user.getId());
+		// waitingRedisRepository.idempotentKeyKeyExists(idempotentKey, ReservationStatus.CANCELLED.name());
 
-		// DB에 상태 값 저장
-		// TODO: 웨이팅 취소 로직 구현 필요 (예: 상태 변경, 취소 시간 기록 등)
+		// DB 웨이팅 상태 취소 처리
+		Reservation reservation = reservationRepository.findReservationByReservationNumber(request.getWaitingNumber())
+			.orElseThrow(ReservationNotFoundException::new);
+
+		reservation.markAsCancelled(LocalDateTime.now());
 
 		// Redis 대기열 취소 이벤트 발행
+		waitingRedisRepository.removeWaiting(storeId, user.getId());
 
-		return;
+		return CancelWaitingResponse.builder()
+			.waitingNumber(reservation.getReservationNumber())
+			.storeId(storeId)
+			.reservationStatus(reservation.getStatus())
+			.canceledAt(reservation.getUpdatedAt())
+			.message("대기 취소가 완료되었습니다.")
+			.build();
 	}
 
 	private String generateWaitingNumber(Long storeId, LocalDateTime timestamp) {
@@ -112,6 +151,7 @@ public class WaitingService {
 		String today = timestamp.format(DateTimeFormatter.BASIC_ISO_DATE); // YYYYMMDD
 
 		// atomic increment
+		// TODO 웨이팅 실패 시 카운터 롤백 처리 필요
 		String dailySeqKey = RedisKeyUtils.buildWaitingSeqKey(storeId) + ":" + today; // ex. waiting:sequence:{storeId}:{today}
 		Long seqNum = waitingRedisRepository.incrementDailySequence(dailySeqKey);
 
