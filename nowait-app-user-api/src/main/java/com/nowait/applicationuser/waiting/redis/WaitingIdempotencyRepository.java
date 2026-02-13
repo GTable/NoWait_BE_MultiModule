3e2f978efdcdeef2dda4e3db80d30be3594856d3
Jihun Kim <zh1229@naver.com>
2026-02-13 17:21:01 +0900

    refactor: 멱등키 서비스 로직 분리

--- nowait-app-user-api/src/main/java/com/nowait/applicationuser/waiting/redis/WaitingIdempotencyRepository.java
+++ nowait-app-user-api/src/main/java/com/nowait/applicationuser/waiting/redis/WaitingIdempotencyRepository.java
@@ -12,9 +12,11 @@ import com.nowait.applicationuser.waiting.dto.WaitingCancelIdempotencyValue;
 import com.nowait.applicationuser.waiting.dto.WaitingIdempotencyValue;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class WaitingIdempotencyRepository {
 
 	private final RedisTemplate<String, String> redisTemplate;
@@ -23,7 +25,7 @@ public class WaitingIdempotencyRepository {
 	private static final Duration TTL = Duration.ofMinutes(10);
 
 	// 멱등키 조회 메서드
-	public Optional<WaitingIdempotencyValue> findByKey(String key) {
+	public Optional<WaitingIdempotencyValue> findByRegisterKey(String key) {
 		String idempotencyValue = redisTemplate.opsForValue().get(key);
 
 		if (idempotencyValue == null) {
@@ -31,26 +33,24 @@ public class WaitingIdempotencyRepository {
 		}
 
 		try {
-			return Optional.of(
-				objectMapper.readValue(idempotencyValue, WaitingIdempotencyValue.class)
-			);
+			log.info("Idempotency value found in Redis for key {}: {}", key, idempotencyValue);
+			return Optional.of(objectMapper.readValue(idempotencyValue, WaitingIdempotencyValue.class));
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Failed to deserialize value from Redis", e);
 		}
 	}
 
 	// 멱등키 조회 메서드
-	public Optional<WaitingCancelIdempotencyValue> findByCancelKey(String key) {
+	public WaitingCancelIdempotencyValue findByCancelKey(String key) {
 		String idempotencyValue = redisTemplate.opsForValue().get(key);
 
 		if (idempotencyValue == null) {
-			return Optional.empty();
+			return null;
 		}
 
 		try {
-			return Optional.of(
-				objectMapper.readValue(idempotencyValue, WaitingCancelIdempotencyValue.class)
-			);
+			log.info("Idempotency value found in Redis for key {}: {}", key, idempotencyValue);
+			return objectMapper.readValue(idempotencyValue, WaitingCancelIdempotencyValue.class);
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Failed to deserialize value from Redis", e);
 		}
@@ -62,6 +62,20 @@ public class WaitingIdempotencyRepository {
 		IdempotencyResponse idempotencyResponse = new IdempotencyResponse(
 			"COMPLETED",
 			response
+		);
+
+		try {
+			String jsonValue = objectMapper.writeValueAsString(idempotencyResponse);
+			redisTemplate.opsForValue().set(key, jsonValue, TTL);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Failed to serialize value for Redis", e);
+		}
+	}
+
+	public void saveIdempotencyInProgress(String key) {
+		IdempotencyResponse idempotencyResponse = new IdempotencyResponse(
+			"IN-PROGRESS",
+			null
 		);
 
 		try {
